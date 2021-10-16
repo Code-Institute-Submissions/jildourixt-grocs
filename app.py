0f47e3d431bbbe6d90049a8f35d4eff7dbff59e2
Jildou de Vries <devriesjildou@gmail.com>
2021-10-16 21:28:25 +0200

    Beautify app.py
    Beautify app.py using PyYapf in Sublime

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
-from flask import (Flask, flash, render_template, 
-	redirect, request, session, url_for)
+from flask import (Flask, flash, render_template, redirect, request, session,
+                   url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
-	import env
-
+    import env
 
 app = Flask(__name__)
 
@@ -23,30 +22,31 @@ def get_items():
     items = mongo.db.items.find()
     return render_template("items.html", items=items)
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
-	if request.method =="POST":
-		# check existence of user instance
-		existing_user = mongo.db.users.find_one(
-			{"username": request.form.get("username").lower()})
-		if existing_user:
-			flash("Username already exists")
-			return redirect(url_for("register"))
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
-		# check if both passwords provided match
-		if request.form.get("password") != request.form.get("password_check"):
-			flash("Passwords do not match")
-			return redirect(url_for("register"))
-		register = {
-			"username": request.form.get("username").lower(),
-			"password": generate_password_hash(request.form.get("password"))
-		}
-		mongo.db.users.insert_one(register)
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
 
-		# put new user into 'session'
-		session["user"] = request.form.get("username").lower()
-		flash("Registration successful.")
-	return redirect(url_for("profile", username=session["user"]))
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
+    return render_template("registration.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -57,10 +57,10 @@ def login():
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+            if check_password_hash(existing_user["password"],
+                                   request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
             else:
                 # invalid password match
                 flash("Password/username incorrect")
@@ -73,14 +73,28 @@ def login():
 
     return render_template("login.html")
 
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # get user's username from db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+    username = mongo.db.users.find_one({"username":
+                                        session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
-	app.run(host=os.environ.get("IP"),
-		port=int(os.environ.get("PORT")),
-		debug=True)												# TODO: remove debug
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)  # TODO: remove debug
